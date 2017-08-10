9572833287922c1befe5eb22aa21170f257f4cbe
Max Gardner <gardner.max@gmail.com>
2017-08-10 11:50:00 -0700

    set default projection

--- mander/districts.py
+++ mander/districts.py
@@ -3,9 +3,9 @@ import geopandas as gpd
 
 class district(object):
 
-    def __init__(self, geojsonFile):
-        self.gdf = gpd.read_file(geojsonFile)
-        self.gdf = self.gdf.to_crs(epsg='2163')
-        self.area = self.gdf.area
-        self.perimeter = self.gdf.length
-
+    def __init__(self, pathToGeojsonOrShapefile, epsg='2163'):
+        self.epsg = epsg
+        self.gdf = gpd.read_file(pathToGeojsonOrShapefile)
+        self.gdf = self.gdf.to_crs(epsg=self.epsg)
+        self.area = self.gdf.area.values[0]
+        self.perimeter = self.gdf.length.values[0]
