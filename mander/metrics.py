191f73a2d6f9e220d85a75f80c07952bb360d9a7
Daniel McGlone <dmcglone@azavea.com>
2017-08-10 14:41:56 -0400

    Update metrics.py with convex hull

--- mander/metrics.py
+++ mander/metrics.py
@@ -4,3 +4,8 @@ import math
 def calculatePolsby(district):
     ppScore = 4 * math.pi * district.area / district.length**2
     return ppScore
+
+
+def calculateConvexHull(district):
+    chScore = district.area / district.convex_hull.area
+    return chScore
