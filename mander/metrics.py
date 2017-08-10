a7dc1d2b09bf25697394d096816d554f859c73d7
Max Gardner <gardner.max@gmail.com>
2017-08-10 13:36:39 -0700

    schwartzberg score

--- mander/metrics.py
+++ mander/metrics.py
@@ -1,5 +1,7 @@
 import math
 import utils
+import numpy as np
+
 
 def calculatePolsby(district):
     ppScore = 4 * math.pi * district.area / district.perimeter**2
@@ -10,7 +12,15 @@ def calculateConvexHull(district):
     chScore = district.area / district.convex_hull.area
     return chScore
 
+
 def calculateReock(district):
     mbcArea = utils.getMinBoundingCircleArea(district)
     reockScore = district.area / mbcArea
     return reockScore
+
+
+def calculateSchwartzberg(district):
+    r = np.sqrt((district.area) / math.pi)
+    c = 2 * math.pi * r
+    score = 1 / (district.perimeter / c)
+    return score
