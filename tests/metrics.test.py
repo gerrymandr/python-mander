648bf409712d8c42417dc217f1c7e3a6292a5b00
Aaron Dennis <adennis@azavea.com>
2017-08-10 14:21:27 -0400

    fix indentation on test

--- tests/metrics.test.py
+++ tests/metrics.test.py
@@ -12,20 +12,20 @@ test_expected = []
 test_results = []
 
 for d in test_districts:
-    
+
   district_boundaries_file = path.join('tests', 'data', d, '.geojson')
   district_scores_file = path.join('tests', 'data', d, '_scores.json')
-  
+
   with json.load(open(district_boundaries_file)) as district_boundary:
-  with json.load(open(district_scores_file)) as district_scores:
-    
-    district = District(district_boundary)
-    
-    test_expected.append(district_scores['polsbypopper'])
-    test_results.append(calculatePolsby(district))
+    with json.load(open(district_scores_file)) as district_scores:
+
+      district = District(district_boundary)
+
+      test_expected.append(district_scores['polsbypopper'])
+      test_results.append(calculatePolsby(district))
 
 
 class TestMetrics(unittest.TestCase):
-      
+
     def test_values(self):
       self.assertEqual(test_expected, test_results)
