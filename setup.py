586d21852133aa5c4ed38fc148f02b577da54917
Richard Barnes <rbarnes@umn.edu>
2017-08-11 09:41:39 -0400

    Formatting

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(name='mander',
-  version='0.1',
-  description='Python package to calculate compactness metrics for district shapes.',
-  url='http://github.com/gerrymandr/python-mander',
-  author='Geometry of Redistricting Workshop',
-  author_email='',
-  license='MIT')
+  version      = '0.1',
+  description  = 'Python package to calculate compactness metrics for district shapes.',
+  url          = 'http://github.com/gerrymandr/python-mander',
+  author       = 'Geometry of Redistricting Workshop',
+  author_email = '',
+  license      = 'MIT'
+)
