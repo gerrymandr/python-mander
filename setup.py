48b6daed85376c5a8911cdc6a706ca5b5f60a89b
Max Gardner <gardner.max@gmail.com>
2017-08-11 12:44:22 -0700

    setup py changes

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='mander',
-  version='0.1',
+  version='0.2',
   description='Python package to calculate compactness metrics for district shapes.',
   url='http://github.com/gerrymandr/python-mander',
   author='Geometry of Redistricting Workshop',
-  author_email='',
-  license='MIT')
+  author_email='gerrymandr@gmail.com',
+  license='MIT',
+  packages=find_packages(exclude=['contrib', 'docs', 'tests*'])
+)
